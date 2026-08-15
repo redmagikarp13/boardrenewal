f1c8b44bcdf70b247d7ad9bc2a3c78947805a5bd
magikarp13 <leonardcoutinho@gmail.com>
2026-08-15 19:43:12 -0300

    feat: sidebar de projetos e topbar moderna

--- plugins/BoardRenewal/Template/sidebar.php
+++ plugins/BoardRenewal/Template/sidebar.php
@@ -1,8 +1,37 @@
+<?php
+$brProjects = $this->BoardRenewalHelper->getUserProjects();
+$brCurrentProjectId = isset($project['id']) ? $project['id'] : 0;
+?>
 <aside class="br-sidebar" id="br-sidebar">
     <div class="br-sidebar__brand">
-        <a href="<?= $this->url->dir() ?>">Kanboard</a>
+        <a href="<?= $this->url->dir() ?>" title="Dashboard">
+            <span class="br-sidebar__logo">◆</span>
+            <span class="br-sidebar__name">Kanboard</span>
+        </a>
     </div>
+
+    <button type="button" class="br-sidebar__search" id="br-search-button" title="Buscar (Ctrl+K)">
+        🔍 <span><?= t('Search') ?></span> <kbd>Ctrl K</kbd>
+    </button>
+
     <nav class="br-sidebar__nav">
-        <?= $this->hook->render('template:dashboard:sidebar') ?>
+        <a class="br-nav-item" href="<?= $this->url->href('DashboardController', 'show', array('user_id' => $this->user->getId())) ?>">
+            <span class="br-nav-item__icon">⊞</span> <?= t('Dashboard') ?>
+        </a>
+
+        <div class="br-sidebar__section-title"><?= t('Projects') ?></div>
+        <?php foreach ($brProjects as $brProject): ?>
+            <a class="br-nav-item <?= $brProject['id'] == $brCurrentProjectId ? 'br-nav-item--active' : '' ?>"
+               href="<?= $this->url->href('BoardViewController', 'show', array('project_id' => $brProject['id'])) ?>">
+                <span class="br-nav-item__icon">▸</span> <?= $this->text->e($brProject['name']) ?>
+            </a>
+        <?php endforeach ?>
     </nav>
+
+    <div class="br-sidebar__footer">
+        <button type="button" class="br-sidebar__mode" id="br-contrast-toggle" title="<?= t('High contrast') ?>">◐</button>
+        <a class="br-nav-item" href="<?= $this->url->href('UserViewController', 'show', array('user_id' => $this->user->getId())) ?>">
+            <span class="br-nav-item__icon">👤</span> <?= $this->text->e($this->BoardRenewalHelper->getCurrentUserFullname()) ?>
+        </a>
+    </div>
 </aside>
